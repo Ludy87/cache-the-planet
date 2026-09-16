07deddc41572aea5e74df6b4879d863742e4efb2
Ludy87 <Ludy87@users.noreply.github.com>
2026-09-16 11:19:23 +0200

    fix: replace existing untrusted cache references

--- src/save.js
+++ src/save.js
@@ -195,7 +195,7 @@ async function deleteUnreferencedObjects(repository, hashes, manifest) {
         return;
       }
     }
-    if (existingReference?.object) {
+    if (existingReference?.object && !untrustedKey) {
       const existingAsset = await c.object(
         repository,
         existingReference.object,
